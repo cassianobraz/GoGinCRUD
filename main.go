5966496a409630d414e8bef9ac58c675455ea72d
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2026-04-24 11:12:47 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/cassianobraz/GoGinCRUD/router"
+import (
+	"github.com/cassianobraz/GoGinCRUD/config"
+	"github.com/cassianobraz/GoGinCRUD/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.NewLogger("main")
+	// Initialize config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	// Initialize router
 	router.Initialize()
 }
